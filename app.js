09fcc3665274cf67eb971164c44d81c8ee24900c
Mashal Afzal <mashal.afzal2996@gmail.com>
2024-05-12 17:16:36 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,3 @@
+function multiplication(num1, num2) { return num1 * num2; }
+var result = multiplication(10, 25);
+console.log(result);
---
+++ app.ts
@@ -0,0 +1,3 @@
+function multiplication (num1: number, num2: number) {return num1 *  num2}
+let result= multiplication (10, 25)
+console.log(result)
